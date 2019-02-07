db2568af1d72498a45c4e178a52eba596623c505
Ker <karvin_jimenez@hotmail.com>
2019-02-07 02:15:23 -0400

    Creacion de la clase EstudianteServicio
    Esta sera la clase con la que relacionare el crud y sera la intermedia entre las demas

---
+++ src/main/java/arkham/knight/pw2/EstudianteServicio.java
@@ -0,0 +1,25 @@
+package arkham.knight.pw2;
+
+import java.util.List;
+
+public class EstudianteServicio implements EstudianteInterface {
+
+    private EstudianteImplementInterface estudianteInterface;
+
+    public EstudianteServicio(){estudianteInterface = new EstudianteImplementInterface(); }
+
+    @Override
+    public void insertar(Estudiante estudiante) {estudianteInterface.insertar(estudiante);}
+
+    @Override
+    public void actualizar(Estudiante estudiante){estudianteInterface.actualizar(estudiante);}
+
+    @Override
+    public void borrar(Estudiante estudiante){estudianteInterface.borrar(estudiante);}
+
+    @Override
+    public List<Estudiante> encontrarEstudiantes(){return estudianteInterface.encontrarEstudiantes();}
+
+    @Override
+    public Estudiante encontrarPorMatricula(int matricula){return estudianteInterface.encontrarPorMatricula(matricula);}
+}
